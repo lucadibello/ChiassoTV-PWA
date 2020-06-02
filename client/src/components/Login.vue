c5abc1f7fa142f601f0ae8275de04be5d3c6599f
Luca Di Bello <luca.dibello@samtrevano.ch>
2020-06-03 00:50:56 +0200

    Added login page

---
+++ client/src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <h1>Effettua il login</h1>
+
+      <input type="email" v-model="email" name="email" placeholder="Email">
+      <input type="password" v-model="password" name="password" placeholder="Password">
+      <br>
+      <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      AuthenticationService.login({
+        'email': this.email,
+        'password': this.password
+      }).then((response) => {
+        console.log(response)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
