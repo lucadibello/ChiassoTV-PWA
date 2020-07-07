842aaf2d714ba8cfd59fa052e41635682215f49e
Luca Di Bello <luca.dibello@samtrevano.ch>
2020-07-07 14:31:53 +0200

    Edited: models now load one by one (not dynamically)

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -12,14 +12,32 @@ const sequelize = new Sequelize(
   config.db.options
 );
 
+/*
+NOTE: LOAD DYNAMICALLY ALL MODELS
+  -> NOT USED BECAUSE THE LOAD ORDER IS WRONG (CAN'T USE FOREIGN KEYS)
+
 fs
   .readdirSync(__dirname)
   .filter((file) => file !== 'index.js')
   .forEach((file) => {
-    console.log(path.join(__dirname, file))
+    console.log('Loaded:', path.join(__dirname, file))
     const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
   })
+*/
+
+// Load user model
+const UserModel = sequelize.import(path.join(__dirname, 'User.js'))
+db[UserModel.name] = UserModel
+
+// Load series model
+const SeriesModel = sequelize.import(path.join(__dirname, 'Serie.js'))
+db[SeriesModel.name] = SeriesModel
+
+// Load episode model
+const EpisodeModel = sequelize.import(path.join(__dirname, 'Episode.js'))
+db[EpisodeModel.name] = EpisodeModel
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
