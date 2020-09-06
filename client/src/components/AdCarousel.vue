a325f86a522bf20f251c015d123205c73cf36013
Luca Di Bello <luca.dibello@samtrevano.ch>
2020-09-07 01:17:24 +0200

    Advertisement carousel component

--- client/src/components/AdCarousel.vue
+++ client/src/components/AdCarousel.vue
@@ -1,9 +1,47 @@
 <template>
-  
+  <carousel :scrollPerPage="true" :autoplay="true" :navigationEnabled="false" 					:paginationEnabled="false" :loop="true" :perPage="4" :perPageCustom="[[0, 1], [768, 1], [800, 2], [950, 3]]">
+
+		<slide v-for="ad in ads" :key="ad.id" :data-index="ad.id">
+			
+			<div v-if="Boolean(ad.website_link)">
+				<!-- Image with link -->
+				<a :href="ad.website_link" target="_blank" rel="sponsored">
+					<b-img class="ad-img" :src="getAdBanner(ad)"/>
+				</a>
+			</div>
+			<div v-else>
+				<!-- Image (without link) -->
+				<b-img class="ad-img" :src="getAdBanner(ad)"/>
+			</div>
+		</slide>
+
+  </carousel>
 </template>
 
 <script>
+/* Import components */
+import { Carousel, Slide } from "vue-carousel";
+
 export default {
-    name: 'AdCarousel'
-}
+  name: "AdCarousel",
+  components: {
+    Carousel,
+    Slide,
+	},
+	props: {
+		ads: Array,
+	},
+	methods : {
+		getAdBanner(item) {
+			return `${process.env.VUE_APP_SERVER_URL}api/ads/${item.id}/banner`
+		}
+	}
+};
 </script>
+
+<style scoped>
+.ad-img {
+	width: 300px !important; 
+	max-width: 100% !important;
+}
+</style>
