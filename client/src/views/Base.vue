484919d8d6da596e5c0706e05a9c6e1bd0e4c6e9
Luca Di Bello <luca.dibello@samtrevano.ch>
2020-09-07 01:17:41 +0200

    Added shuffle method in parent

--- client/src/views/Base.vue
+++ client/src/views/Base.vue
@@ -25,6 +25,26 @@ export default {
       isOnline: navigator.onLine || true
     }
   },
+  methods: {
+    shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+  },
   mounted () {
     // Connection changed to online
     window.addEventListener('online', () => {
