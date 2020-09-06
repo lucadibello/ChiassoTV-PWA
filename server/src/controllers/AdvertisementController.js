c7e290b5caff77c61b568fe84c677391904eb81a
Luca Di Bello <luca.dibello@samtrevano.ch>
2020-09-06 23:41:26 +0200

    Added new ad management features

--- server/src/controllers/AdvertisementController.js
+++ server/src/controllers/AdvertisementController.js
@@ -1,111 +1,250 @@
-const config = require('../config/config');
-const {Advertisement} = require('../models')
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+const config = require("../config/config");
+const { Advertisement, Sequelize } = require("../models");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 // Ads images storage
-const adsStorage = multer.diskStorage(
-    {
-        destination: config.assets.ads,
-        filename: function (req, file, cb ) {
-            // Set filename
-            cb( null, file.originalname );
-        }
-    }
-);
+const adsStorage = multer.diskStorage({
+  destination: config.assets.ads,
+  filename: function (req, file, cb) {
+    // Set filename
+    cb(null, file.originalname);
+  },
+});
 
 // Create multer object
 const uploadBanner = multer({
-    limits: { files: 1},
-    storage: adsStorage
-}).single('banner')
+  limits: { files: 1 },
+  storage: adsStorage,
+}).single("banner");
+
+function getCurrentDate() {
+	let today = new Date();
+	return today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+}
 
 module.exports = {
-    get (req, res) {
-        Advertisement.findAll().then(ads => {
-            if (!ads) {
-                // Return error message
-                res.status(400).send({
-                    error: 'Cannot load ads, database error'
-                })
-            } else {
-                // Send back ads
-                res.send(ads)
-            }
-        })
-    },
-    add (req, res) {
-        // Validation step completed!
-
-        // Save data into DB using Advertisement model
-        Advertisement.create({
-            client_email: req.body.client_email,
-            client_name: req.body.client_name,
-            client_surname: req.body.client_surname,
-            client_phoneNumber: req.body.client_phoneNumber,
-            img: req.body.img,
-            start_date: req.body.start_date,
-            end_date: req.body.end_date,
-            description: req.body.description,
-            website_link: req.body.website_link
-        }).then(ad => {
-            if (ad) {
-                res.send({message: 'Ad created and published successfully'})
-            } else {
-                res.status(400).send({error: 'Error occurred during the ad creation process'});
-            }
-        }).catch((err) => {
-            // Return error message
-            res.status(400).send({error: err.message})
-        })
-    },
-    upload (req, res) {
-        // Upload banner using Multer
-        uploadBanner(req, res, function (err) {
-            if (err instanceof multer.MulterError || err) {
-                // A Multer error occurred when uploading.
-                res.status(400).send({
-                    message: err.message
-                })
-            }
-            else {
-                // Send success response (message + file infos)
-                res.send({
-                    message: 'Ad banner uploaded successfully on the server',
-                    file: req.file
-                })
-            }
+  get(req, res) {
+    Advertisement.findAll().then((ads) => {
+      if (!ads) {
+        // Return error message
+        res.status(400).send({
+          error: "Cannot load ads, database error",
         });
-    },
-    undoUpload (req, res) {
-        // Get file name from request
-        const filename = JSON.parse(req.body).file.filename
+      } else {
+        // Send back ads
+        res.send(ads);
+      }
+    });
+	},
+	getInbound (req, res) {
+		const currentDate = getCurrentDate()
+		
+		Advertisement.findAll({
+      where: {
+        start_date: {
+          [Sequelize.Op.gt]: currentDate
+				},
+				end_date: {
+					[Sequelize.Op.gte]: currentDate
+				}
+      },
+    }).then((result) => {
+			if (!result) {
+				// Return error message
+        res.status(400).send({
+          error: "Cannot load inbound ads, database error",
+        });
+			} else {
+				// Send back ads
+        res.send(result);
+			}
+		});
+	},
+	getExpired (req, res) {
+		const currentDate = getCurrentDate()
 
-            // Delete flag (false = not deleted, true = file deleted successfully)
-            let wasDeleted = false;
+		Advertisement.findAll({
+			where: {
+				end_date: {
+					[Sequelize.Op.lt]: currentDate	
+				}
+			}
+		}).then(ads => {
+			if (!ads) {
+				// Return error message
+				res.status(400).send({
+					error: "Cannot load expired ads, database error",
+				});
+			}	else {
+				// Send back ads
+        res.send(ads);
+			}
+		})
+	},
+  getAvailable(req, res) {
+		const currentDate = getCurrentDate()
+		
+    Advertisement.findAll({
+      where: {
+        start_date: {
+          [Sequelize.Op.tle]: currentDate
+				},
+				end_date: {
+					[Sequelize.Op.gte]: currentDate
+				}
+      },
+    }).then((result) => {
+			if (!result) {
+				// Return error message
+        res.status(400).send({
+          error: "Cannot load available ads, database error",
+        });
+			} else {
+				// Send back ads
+        res.send(result);
+			}
+		});
+  },
+  sendBanner (req, res) {
+    Advertisement.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(ad => {
+      if (!ad) {
+        // Return error message
+        res.status(404).send({
+          error: "Cannot load ad's banner, unknown error",
+        });
+      } else {
+        // Look for image in folder
+        let files = fs.readdirSync(config.assets.ads);
 
-            // Cycle all files inside episode temp directory
-            const files = fs.readdirSync(path.resolve(config.assets.ads))
-            console.log(files)
-            // Cycle throght all files present in the directory
-            files.forEach((file) => {
-                // Find file
-                if (file === filename) {
-                    // Delete file
-                    fs.unlinkSync(path.resolve(config.assets.ads, filename))
-                    
-                    // Set wasDeleted flag
-                    wasDeleted = true;
-                }
-            })
+        // Check if banner exists
+        if (files.includes(ad.img)) {
+          // Send back image file
+          res.sendFile(path.resolve(config.assets.ads, ad.img))
+        } else {
+          res.status(404).send({
+            error: "Cannot locate ad's banner on the server"
+          })
+        }
+      }
+    })
+  },
+  delete (req, res) {
+    // Get ad's record
+    Advertisement.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['img']
+    }).then(banner => {
+      if (!banner) {
+        // Return error message
+        res.status(400).send({
+          error: "Cannot fetch ad information, database error",
+        });
+      } else {
+        // Got banner filename
+        fs.unlinkSync(path.resolve(config.assets.ads, banner.img))
 
-            if (wasDeleted) {
-                // Send response back
-                res.send({message: 'Ad banner deleted successfully'})
-            } else {
-                res.status(400).send({error: 'Unable to locate the ad banner file'})
+        // Remove DB record
+        Advertisement.destroy({
+          where: {
+              id: req.params.id,
+          }
+        }).then(result => {
+            if(result){
+                res.send({
+                    message: "Ad deleted successfully"
+                })
             }
-        
+            else{
+                res.status(400).send({
+                    error: `The specified ad does not exists (Identifier number: ${req.params.id})`
+                })
+            }
+        })
+
+      }
+    })
+  },
+  add(req, res) {
+    // Validation step completed!
+
+    // Save data into DB using Advertisement model
+    Advertisement.create({
+      client_email: req.body.client_email,
+      client_name: req.body.client_name,
+      client_surname: req.body.client_surname,
+      client_phoneNumber: req.body.client_phoneNumber,
+      img: req.body.img,
+      start_date: req.body.start_date,
+      end_date: req.body.end_date,
+      description: req.body.description,
+      website_link: req.body.website_link,
+    })
+      .then((ad) => {
+        if (ad) {
+          res.send({ message: "Ad created and published successfully" });
+        } else {
+          res
+            .status(400)
+            .send({ error: "Error occurred during the ad creation process" });
+        }
+      })
+      .catch((err) => {
+        // Return error message
+        res.status(400).send({ error: err.message });
+      });
+  },
+  upload(req, res) {
+    // Upload banner using Multer
+    uploadBanner(req, res, function (err) {
+      if (err instanceof multer.MulterError || err) {
+        // A Multer error occurred when uploading.
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        // Send success response (message + file infos)
+        res.send({
+          message: "Ad banner uploaded successfully on the server",
+          file: req.file,
+        });
+      }
+    });
+  },
+  undoUpload(req, res) {
+    // Get file name from request
+    const filename = JSON.parse(req.body).file.filename;
+
+    // Delete flag (false = not deleted, true = file deleted successfully)
+    let wasDeleted = false;
+
+    // Cycle all files inside episode temp directory
+    const files = fs.readdirSync(path.resolve(config.assets.ads));
+    console.log(files);
+    // Cycle throght all files present in the directory
+    files.forEach((file) => {
+      // Find file
+      if (file === filename) {
+        // Delete file
+        fs.unlinkSync(path.resolve(config.assets.ads, filename));
+
+        // Set wasDeleted flag
+        wasDeleted = true;
+      }
+    });
+
+    if (wasDeleted) {
+      // Send response back
+      res.send({ message: "Ad banner deleted successfully" });
+    } else {
+      res.status(400).send({ error: "Unable to locate the ad banner file" });
     }
-}
+  },
+};
--- server/src/routes/api/advertisement.js
+++ server/src/routes/api/advertisement.js
@@ -7,11 +7,27 @@ const router = express.Router()
 const AdvertisementController = require('../../controllers/AdvertisementController')
 const AdvertisementControllerPolicy = require('../../policies/AdvertisementControllerPolicy')
 
-// Upload series banner
+// Get all banners
 router.get('/', AdvertisementController.get)
+
+// Get only available ads
+router.get('/available', AdvertisementController.getAvailable)
+// Get only expired ads
+router.get('/expired', AdvertisementController.getExpired)
+// Get only inbound ads
+router.get('/inbound', AdvertisementController.getInbound)
+
+// Delete Ad
+router.delete('/:id', AdvertisementController.delete)
+
+// Create new banner
 router.post('/', JwtHelper.authenticateToken, AdvertisementControllerPolicy.add, AdvertisementController.add)
 
+// Upload series banner
 router.post('/upload', JwtHelper.authenticateToken, AdvertisementController.upload)
+// Revert upload
 router.delete('/upload', JwtHelper.authenticateToken, AdvertisementController.undoUpload)
+// Fetch banner image
+router.get('/:id/banner', AdvertisementController.sendBanner)
 
 module.exports = router;
